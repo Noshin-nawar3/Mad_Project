ccd08c53eb6d28c41eb696adc68f78184779ce1f
Sadia Afrin <afrinzzxx@gmail.com>
2025-08-17 23:25:49 +0600

    Bookmark page

--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -1,19 +1,17 @@
 import { Stack } from "expo-router";
 import HomeHeader from "../../components/HomeHeader";
+import { BookmarkProvider } from "./BookmarkContext";
 // Change this:
 
 // To this (adjust the path based on your folder structure):
 export default function _layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown : false
-      }}
-    />
+    <BookmarkProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </BookmarkProvider>
   )
   // return <Stack />;
 }
-
 
 /*
 
