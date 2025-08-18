3eac0e23960237b8c35c33a2b51617f472e54518
mrimmoyee <mrimmoyee20@gmail.com>
2025-08-19 00:01:03 +0600

    quiz data

--- app/(app)/initFirebaseData.js
+++ app/(app)/initFirebaseData.js
@@ -1,2 +1,13 @@
 // import { collection } from "firebase/firestore";
 // import { db } from "../../firebaseConfig";
+const initializeQuestions = async () => {
+  const questions = [
+    // Science
+    {
+      subject: 'Science',
+      level: 'Level 1',
+      question: 'What do plants need to grow big and strong?',
+      options: ['Water, sunlight, and air', 'Soil, rocks, and wind', 'Darkness, cold, and rain'],
+      correctAnswer: 'Water, sunlight, and air',
+    },
+    
