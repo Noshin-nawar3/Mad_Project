2c9b89ad7e232a4fc711fc3b215bef248d432518
mrimmoyee <mrimmoyee20@gmail.com>
2025-08-19 00:01:23 +0600

    quiz data

--- app/(app)/initFirebaseData.js
+++ app/(app)/initFirebaseData.js
@@ -10,4 +10,19 @@ const initializeQuestions = async () => {
       options: ['Water, sunlight, and air', 'Soil, rocks, and wind', 'Darkness, cold, and rain'],
       correctAnswer: 'Water, sunlight, and air',
     },
+    {
+      subject: 'Science',
+      level: 'Level 1',
+      question: 'What do we breathe in to stay alive?',
+      options: ['Oxygen', 'Carbon dioxide', 'Nitrogen'],
+      correctAnswer: 'Oxygen',
+    },
+    // Mathematics
+    {
+      subject: 'Mathematics',
+      level: 'Level 1',
+      question: 'Jackson has five pieces of candy in his lunch. Fred has two fewer candies than Jackson. How many candies does Fred have?',
+      options: ['2', '3', '5', '7'],
+      correctAnswer: '3',
+    },
     
