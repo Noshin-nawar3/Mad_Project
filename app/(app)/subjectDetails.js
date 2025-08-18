1361d557d6a950c7975034a3d907ed81db77ee7e
mrimmoyee <mrimmoyee20@gmail.com>
2025-08-18 21:31:28 +0600

    subjectdetails

---
+++ app/(app)/subjectDetails.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View } from 'react-native';
+import HomeHeader from "../../components/HomeHeader";
+
+export default function QuizScreen() {
+  
+
+  return (
+    <View style={styles.container_home}> 
+            <HomeHeader />
+            <Text style={styles.title}>Quiz Screen</Text>
+          </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container_home: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#DBEAFE',
+    padding: 20,
+    justifyContent: 'center',
+  },
+  title: {
+    marginTop: 0,
+    fontSize: 24,
+    fontWeight: "bold",
+    marginLeft: 30,
+  },
+
+  button: {
+    backgroundColor: '#2563eb',
+    padding: 15,
+    borderRadius: 8,
+    marginVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonSection: {
+    marginTop: 20,
+    paddingHorizontal: 16,
+    width: "100%",
+    gap: 16,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 10,
+    flexWrap: "wrap",
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginLeft: 8,
+  },
+  icon: {
+    marginRight: 4,
+  },
+});
