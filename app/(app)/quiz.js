73f727c510620472fd482cf7cc9ffdb872aa8dd9
mrimmoyee <mrimmoyee20@gmail.com>
2025-08-18 23:56:09 +0600

    ass quiz.js

---
+++ app/(app)/quiz.js
@@ -0,0 +1,6 @@
+// import { useLocalSearchParams } from "expo-router";
+// import { collection, getDocs, query, where } from "firebase/firestore";
+// import { useEffect, useState } from "react";
+// import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+// import { db } from "../../firebaseConfig";
+
