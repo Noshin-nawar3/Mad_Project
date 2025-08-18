151e0521f77e1119054d02b47c73af3458cf38a7
mrimmoyee <mrimmoyee20@gmail.com>
2025-08-18 23:56:31 +0600

    quiz

--- app/(app)/quiz.js
+++ app/(app)/quiz.js
@@ -4,3 +4,10 @@
 // import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 // import { db } from "../../firebaseConfig";
 
+export default function Quiz() {
+  const { subject, level } = useLocalSearchParams();
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState({});
+  const [showResults, setShowResults] = useState(false);
+
+  
