c8761a3779f8737e769c1e842519a559576cf404
mrimmoyee <mrimmoyee20@gmail.com>
2025-08-19 00:00:10 +0600

    quiz

--- app/(app)/quiz.js
+++ app/(app)/quiz.js
@@ -1,8 +1,8 @@
-// import { useLocalSearchParams } from "expo-router";
-// import { collection, getDocs, query, where } from "firebase/firestore";
-// import { useEffect, useState } from "react";
-// import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
-// import { db } from "../../firebaseConfig";
+import { useLocalSearchParams } from "expo-router";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+import { db } from "../../firebaseConfig";
 
 export default function Quiz() {
   const { subject, level } = useLocalSearchParams();
